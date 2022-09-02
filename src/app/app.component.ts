c185eff4a162a2bb9cb60fa068d8f381b0dee570
Mayara Silva <devmayara11@gmail.com>
2022-09-02 18:35:37 -0300

    criado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Tarefa } from './tarefa';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'Angular_to-do';
+  title = 'Lista de Tarefas';
+
+  tarefas = [
+    new Tarefa('Fazer Bolo'),
+    new Tarefa('Testar o app')
+  ];
+
+  addTarefa = (tarefa: string) => {
+    this.tarefas.push( new Tarefa(tarefa) );
+  }
 }
